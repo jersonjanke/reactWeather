a0e5697a810d9a7e28a7fd4abe534a32ed7f3391
jerson.janke <jerson.janke@senior.com.br>
2017-09-12 12:20:46 -0300

    Add styles to weather page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h1 className="text-center">Examples Component</h1>
+      <h1 className="text-center">Examples</h1>
       <p>Here are a few Examples locations to try out:</p>
       <ol>
         <li>
